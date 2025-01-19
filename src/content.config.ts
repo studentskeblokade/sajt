69dfc29b11e45b25ec3535afb5e69dc1f9c7c9f8
studentskeblokade <studentskeblokade@gmai.com>
2025-01-20 00:49:21 +0100

    Odvojene vesti i akcije

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,9 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
-const news = defineCollection({
-	// Load Markdown and MDX files in the `src/content/news/` directory.
-	loader: glob({ base: './src/content/news', pattern: '**/*.{md,mdx}' }),
+const vesti = defineCollection({
+	// Load Markdown and MDX files in the `src/content/vesti/` directory.
+	loader: glob({ base: './src/content/vesti', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
@@ -15,4 +15,19 @@ const news = defineCollection({
 	}),
 });
 
-export const collections = { news };
+const akcije = defineCollection({
+	// Load Markdown and MDX files in the `src/content/vesti/` directory.
+	loader: glob({ base: './src/content/akcije', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date().optional(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+
+export const collections = { vesti, akcije };
---
+++ src/content/vesti/ogradjivanje.md
@@ -0,0 +1,9 @@
+---
+title: Ogradjivanje
+description: Lorem ipsum dolor sit amet
+pubDate: 2024-12-11
+heroImage: /images/blog-placeholder-3.jpg
+---
+
+
+OGRADJUJEMO SE
